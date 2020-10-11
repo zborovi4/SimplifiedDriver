de5f3d377741cf8ce95b8a3a817138c0c91453f6
ZborovskiyA <zborovi4@gmail.com>
2020-10-11 23:32:55 +0300

    Added Commands class and first method for "Text" command

---
+++ SimplifiedDriver/Commands.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplifiedDriver
+{
+    static class Commands
+    {
+         static public void Text(string text) // Command "Text" - Text Output
+        {
+            Console.Clear();
+            Console.WriteLine(text);
+        }
+    }
+}
--- SimplifiedDriver/Program.cs
+++ SimplifiedDriver/Program.cs
@@ -12,66 +12,83 @@ namespace SimplifiedDriver
         static void Main(string[] args)
         {
             string text = null;
-            Console.Write("Text: ");
+            Console.Write("Command: ");
             bool flag = false;
-            while (flag == false)
+            try
             {
-                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-                int code = keyInfo.KeyChar;
-                if (text != null && text.Length >= 1 && code == 8) // условие для бекспейса
+                while (flag == false)
                 {
-                    text = text.Remove(text.Length - 1);
-                    Console.Clear();
-                    Console.Write($"Text: {text}");
-                }
-
-                if (code >= 32 && code <= 127) // разрешенный диапазон по таблице ASCII входных символов
-                {
-                    char ch = keyInfo.KeyChar;
-                    text += ch;
-                    Console.Write(ch);
-                    if (ch == 'E' && text[text.Length-2] == ':') // окнчание функции
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                    int code = keyInfo.KeyChar;
+                    if (text != null && text.Length >= 1 && code == 8) // Condition for backspace
                     {
-                        flag = true;
+                        text = text.Remove(text.Length - 1);
+                        Console.Clear();
+                        Console.Write($"Text: {text}");
+                    }
+
+                    if (code >= 32 && code <= 127) // Allowed range of ASCII input characters
+                    {
+                        char ch = keyInfo.KeyChar;
+                        text += ch;
+                        Console.Write(ch);
+                        if (ch == 'E' && text[text.Length - 2] == ':') // Condition end of function
+                        {
+                            flag = true;
+                        }
                     }
                 }
             }
-            //string text = "PS:1,0,255:E";
-            string defineFunction = @"(P\S:\S*:E$)";
-            string command = null;
-            Match match = Regex.Match(text, defineFunction);
-            if(match.Success)
+            catch(Exception e)
             {
-                Console.WriteLine(match.Groups[1].Value);
-                command = match.Groups[1].Value;
+                Console.WriteLine(e.Message);
             }
-            char key = command[1];
-            switch (key)
+           
+            
+            try
             {
-                case 'T':
-                    Console.WriteLine("Command 'T'");
-                    Match argument = Regex.Match(command, @":(\w*):");
-                    Console.WriteLine($"Argumet: {argument.Groups[1].Value}");
-                    break;
-                case 'S':
-                    Console.WriteLine("Command 'S'");
-                    Match arguments = Regex.Match(command, @":(\d*),(\d*):");
-                    if (arguments.Success)
-                    {
-                        int a = Convert.ToInt32(arguments.Groups[1].Value);
-                        int b = Convert.ToInt32(arguments.Groups[2].Value);
-                        Console.WriteLine($"Argumet: {a}, {b}");
-                    }
-                    Console.WriteLine("Не верный формат аргументов");
-                    break;
-                default:
-                    Console.WriteLine("Совпадений не найдено");
-                    break;
+                //string text = "PS:1,0,255:E";
+                string defineFunction = @"(P\S:\S*:E$)";
+                string command = null;
+                Match match = Regex.Match(text, defineFunction);
+                if (match.Success)
+                {
+                    Console.WriteLine(match.Groups[1].Value);
+                    command = match.Groups[1].Value;
+                }
+                char key = command[1];
+                switch (key)
+                {
+                    case 'T':           // Command "Text" - Text Output
+                        Match argument = Regex.Match(command, @":(\w*):");
+                        if (argument.Success)
+                        {
+                            Commands.Text(argument.Groups[1].Value);
+                        }
+                        else
+                            Console.WriteLine("The \"Text\" command argument is invalid");
+                        break;
+                    case 'S':
+                        Console.WriteLine("Command 'S'");
+                        Match arguments = Regex.Match(command, @":(\d*),(\d*):");
+                        if (arguments.Success)
+                        {
+                            int a = Convert.ToInt32(arguments.Groups[1].Value);
+                            int b = Convert.ToInt32(arguments.Groups[2].Value);
+                            Console.WriteLine($"Argumet: {a}, {b}");
+                        }
+                        Console.WriteLine("Не верный формат аргументов");
+                        break;
+                    default:
+                        Console.WriteLine("Совпадений не найдено");
+                        break;
 
+                }
             }
-
-
-
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            } 
         }
     }
 }
